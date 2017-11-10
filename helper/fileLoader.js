e290b825a1496cdd48e497e3869878cc05316658
H.T. Koo <h1017760@connect.hku.hk>
2017-11-10 14:15:14 +0800

    added async loading in fileLoader.js (promiseLoad) react-native: hey-simon-game

--- helper/fileLoader.js
+++ helper/fileLoader.js
@@ -4,7 +4,23 @@ const path = require('path');
 module.exports = {
     load: {
         fromPathAsString(params) {
-            return fs.readFileSync(path.normalize(`${params.root}/${params.relativePath}`)).toString();
+            return fs.readFileSync(getPathFromParams(params)).toString();
+        }
+    },
+    promiseLoad: {
+        fromPathAsString(params) {
+            return new Promise((resolve, reject) => {
+                fs.readFile(getPathFromParams(params), (err, data) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    return resolve(data.toString());
+                });
+            });
         }
     }
 };
+
+function getPathFromParams(params) {
+    return path.normalize(`${params.root}/${params.relativePath}`);
+}
--- helper/tests/fileLoader.test.js
+++ helper/tests/fileLoader.test.js
@@ -5,7 +5,7 @@ const fileLoader = require("../fileLoader");
 describe("fileLoader", function () {
     describe("load", function () {
         describe("fromPathAsString", function () {
-            it("should load file from root + relative path", function () {
+            it("should load file from root + relative path and return string", function () {
                 // given
                 let expectedTest = "someText";
 
@@ -17,6 +17,24 @@ describe("fileLoader", function () {
 
                 // then
                 chai.expect(actualText).to.equal(expectedTest);
+            });
+        });
+    });
+
+    describe("promiseLoad", function () {
+        describe("fromPathAsString", function () {
+            it("should load file from root + relative path and return promise", function () {
+                // given
+                let expectedTest = "someText";
+
+                // when
+                let promise = fileLoader.promiseLoad.fromPathAsString({
+                    root: __dirname,
+                    relativePath: `../resources/someFile.txt`
+                });
+
+                // then
+                return promise.then(actualText => chai.expect(actualText).to.equal(expectedTest));
             });
         });
     });
