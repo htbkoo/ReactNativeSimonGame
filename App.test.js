f66d81dfc2e5fcceb615718165f2f75429a93a69
H.T. Koo <h1017760@connect.hku.hk>
2017-11-10 14:54:43 +0800

    updated App.test.js react-native: hey-simon-game

--- App.test.js
+++ App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import chai from "chai"
 import App from './App';
 
 import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
+describe('App.test.js', function () {
+    it('renders without crashing', () => {
+        const rendered = renderer.create(<App/>).toJSON();
+        chai.expect(rendered).to.equal(true);
+    });
 });
