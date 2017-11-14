75eb191a52a32f488fcb9f7ead810183687686e0
htbkoo <h1017760@connect.hku.hk>
2017-11-14 23:47:24 +0800

    react-native-windows project: updated build_a_simon_game/export; react-native: hey-simon-game
    --HG--
    branch : windows

--- build_a_simon_game/export.js
+++ build_a_simon_game/export.js
@@ -1,7 +1,7 @@
 const SimonGameApp = require("./src/SimonGameApp").default,
-    game = require("./src/game").default;
+    Game = require("./src/game").default;
 
 module.exports = {
     SimonGameApp,
-    game
+    Game
 };
